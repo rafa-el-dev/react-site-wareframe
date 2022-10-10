42cd64e9a26893ac729f32495d220b5d9a219b9b
Rafael Ribeiro <rafael.ribeiro.nasci@gmail.com>
2022-10-10 15:00:10 -0300

    feat: add Menu component

---
+++ src/components/Menu/index.js
@@ -0,0 +1,16 @@
+import Button from "../Button"
+
+function Menu() {
+
+  return (
+    <div className="menu">
+      <Button>Menu Item</Button>
+      <Button>Menu Item</Button>
+      <Button>Menu Item</Button>
+      <Button>Menu Item</Button>
+      <Button primary>Call to action</Button>
+    </div>
+  )
+}
+
+export default Menu
